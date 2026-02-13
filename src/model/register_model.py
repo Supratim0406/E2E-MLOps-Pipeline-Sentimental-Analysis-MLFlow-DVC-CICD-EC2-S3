90cf9980a4662dd27b43a8252fcca97b67879194
Supratim Saha <supratimsmail@gmail.com>
2026-02-14 01:51:18 +0530

    Updated project files for CICD

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -21,8 +21,8 @@ os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
-repo_owner = "vikashdas770"
-repo_name = "YT-Capstone-Project"
+repo_owner = "Supratim0406"
+repo_name = "E2E-MLOps-Pipeline-Sentimental-Analysis-MLFlow-DVC-CICD-EC2-S3"
 
 # Set up MLflow tracking URI
 mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
